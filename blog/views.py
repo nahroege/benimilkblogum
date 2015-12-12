8866e7feb80fb13d8832420e427e9dca2bf00107
ege orhan <egeorhan@yandex.com>
2015-12-12 17:51:10 +0200

    yeni postlar eklendi

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-def post_list(request):     
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post  
+def post_list(request):
+    posts = Post.objects.filter(yaratilis_tarihi__lte=timezone.now()).order_by('yaratilis_tarihi')
+    return render(request, 'blog/post_list.html', {'posts':posts})
